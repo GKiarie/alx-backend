e67f9e2a2a0f513064e8a4fea10c747c4f10ae2f
GKiarie <igitaukiarie@gmail.com>
2023-07-22 11:35:48 +0300

    updated helper ftn

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,10 +5,10 @@ arguments page and page_size
 """
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> tuple:
     """
     ftn takes in two ints and returns atuple
     """
     start_index: int = (page - 1) * page_size
-    end_index: int = page * page_size
+    end_index: int = start_index + page_size
     return (start_index, end_index)
